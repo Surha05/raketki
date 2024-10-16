5940c7717a0d5a8100aba6b7461e328c76111357
Surha <surha_dag@mail.ru>
2024-10-16 21:27:48 +0300

    правки

--- client/game/game.js
+++ client/game/game.js
@@ -1,6 +1,7 @@
 const log = console.log;
 
 const canvas = document.querySelector('#canvas');
+const modal = document.querySelector('.modal__wrapper');
 const ctx = canvas.getContext('2d');
 const body = document.body;
 let width = getComputedStyle(canvas).width;
@@ -21,6 +22,7 @@ let player1 = {
   xSpeed: 0,
   ySpeed: 0,
   maxSpeed: 6,
+  score: 0,
   draw() {
     this.move();
     this.setCoordinates();
@@ -84,6 +86,7 @@ let player2 = {
   xSpeed: 0,
   ySpeed: 0,
   maxSpeed: 6,
+  score: 0,
   draw() {
     this.move();
     this.setCoordinates();
@@ -143,21 +146,30 @@ let ball = {
   r: 20,
   x: canvas.width/2,
   y: canvas.height/2,
-  xSpeed: 2,
-  ySpeed: 2,
+  xSpeed: 6,
+  ySpeed: 6,
   maxSpeed: 0,
 
   move() {
-    if(this.x - this.r < 0 ) this.xSpeed = Math.abs(this.ySpeed);
-    if(this.x + this.r > canvas.width ) this.xSpeed = -Math.abs(this.ySpeed);
-    if(this.y - this.r < 0 ) this.ySpeed = Math.abs(this.ySpeed);
-    if(this.y + this.r > canvas.height ) this.ySpeed = -Math.abs(this.ySpeed);
+    if(this.x - this.r < 0 ) this.xSpeed = Math.abs(this.xSpeed);
+    if(this.x + this.r > canvas.width ) this.xSpeed = -Math.abs(this.xSpeed);
+    if(this.y + this.r < 0 ) {
+      score.player1++;
+      this.y = canvas.height/2;
+      this.ySpeed = Math.abs(this.ySpeed);
+    }
+    if(this.y - this.r > canvas.height ) {
+      score.player2++;
+      this.y = canvas.height/2;
+      this.ySpeed = -Math.abs(this.ySpeed);
+    }
 
     this.x += this.xSpeed;
     this.y += this.ySpeed;
   },
   draw() {
-    this.collision();
+    this.collisionPlayer1();
+    this.collisionPlayer2();
     this.move();
     ctx.beginPath();
     ctx.save();
@@ -167,27 +179,94 @@ let ball = {
     ctx.closePath();
     ctx.restore();
   },
-  collision() {
-    if(this.y + this.r > player1.y
-      && this.y - this.r < player1.y + player1.h
-      && this.x + this.r > player1.x
-      && this.x - this.r < player1.x + player1.w
-    ) {
-      if(this.y + this.r > player1.y) {
-        this.ySpeed = -Math.abs(this.ySpeed);
-        player1.ySpeed = Math.abs(player1.ySpeed);
-      }
-      if(this.y - this.r < player1.y + player1.h) {
-        this.ySpeed = Math.abs(this.ySpeed);
-        player1.ySpeed = -Math.abs(player1.ySpeed);
-      }
+  collisionPlayer1() {
+    if(this.y+this.r < player1.y) return;
+    if(this.y-this.r > player1.y+player1.h) return;
+    if(this.x+this.r < player1.x) return;
+    if(this.x-this.r > player1.x+player1.w) return;
+    // столкновение низа мяча и верха player 1
+    if(this.y < player1.y && this.x > player1.x && this.x < player1.x + player1.w ) {
+      this.ySpeed = -Math.abs(this.ySpeed);
+      player1.ySpeed = Math.abs(player1.ySpeed);
     }
-  }
+    // столкновение верха мяча и низа player 1
+    if(this.y > player1.y+player1.h && this.x > player1.x && this.x < player1.x + player1.w ) {
+      this.ySpeed = Math.abs(this.ySpeed);
+      player1.ySpeed = -Math.abs(player1.ySpeed);
+    }
+    // столкновение левой стороны мяча и правой стороны player 1
+    if(this.x > player1.x+player1.w && this.y > player1.y && this.y < player1.y + player1.h ) {
+      this.xSpeed = Math.abs(this.xSpeed);
+      player1.xSpeed = -Math.abs(player1.xSpeed);
+    }
+    // столкновение правой стороны мяча и левой стороны player 1
+    if(this.x < player1.x && this.y > player1.y && this.y < player1.y + player1.h ) {
+      this.xSpeed = -Math.abs(this.xSpeed);
+      player1.xSpeed = Math.abs(player1.xSpeed);
+    }
+  },
+  collisionPlayer2() {
+    if(this.y+this.r < player2.y) return;
+    if(this.y-this.r > player2.y+player2.h) return;
+    if(this.x+this.r < player2.x) return;
+    if(this.x-this.r > player2.x+player2.w) return;
+    // столкновение низа мяча и верха player2
+    if(this.y < player2.y && this.x > player2.x && this.x < player2.x + player2.w ) {
+      this.ySpeed = -Math.abs(this.ySpeed);
+      player2.ySpeed = Math.abs(player2.ySpeed);
+    }
+    // столкновение верха мяча и низа player2
+    if(this.y > player2.y+player2.h && this.x > player2.x && this.x < player2.x + player2.w ) {
+      this.ySpeed = Math.abs(this.ySpeed);
+      player2.ySpeed = -Math.abs(player2.ySpeed);
+    }
+    // столкновение левой стороны мяча и правой стороны player2
+    if(this.x > player2.x+player2.w && this.y > player2.y && this.y < player2.y + player2.h ) {
+      this.xSpeed = Math.abs(this.xSpeed);
+      player2.xSpeed = -Math.abs(player2.xSpeed);
+    }
+    // столкновение правой стороны мяча и левой стороны player2
+    if(this.x < player2.x && this.y > player2.y && this.y < player2.y + player2.h ) {
+      this.xSpeed = -Math.abs(this.xSpeed);
+      player2.xSpeed = Math.abs(player2.xSpeed);
+    }
+  },
 };
+let score = {
+  player1: 0,
+  player2: 0,
+  draw() {
+    let fontSize = 260;
+    if(this.player1 >= 10) {
+      this.player1 = 'Победа';
+      this.player2 = 'Позор';
+      pause = true;
+      fontSize = 150;
+      modal.style.display = 'flex';
+    }
+    if(this.player2 >= 10) {
+      this.player2 = 'Победа';
+      this.player1 = 'Позор';
+      pause = true;
+      fontSize = 150;
+      modal.style.display = 'flex';
+    }
+    ctx.save();
+    ctx.fillStyle = '#20dda1';
+    ctx.font = `${fontSize}px sans-serif`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'top';
+    ctx.fillText(this.player2, canvas.width/2, 80);
+    ctx.textBaseline = 'bottom';
+    ctx.fillText(this.player1, canvas.width/2, canvas.height - 70);
+    ctx.restore();
+  },
+}
 
 // VARIABLEs
 let frame = 0;
 let keyPressed = new Set();
+let pause = false;
 
 // EVENTs
 document.addEventListener('keydown', (e) => {
@@ -196,13 +275,16 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('keyup', e => {
   keyPressed.delete(e.code);
 });
+modal.addEventListener('click', clickModal);
 
 game();
 
 // FUNCTIONs
 function game() {
+  if(pause) return;
   frame++;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  score.draw();
   drawLine();
   player1.draw();
   player2.draw();
@@ -214,4 +296,9 @@ function drawLine() {
   ctx.fillStyle = '#333';
   ctx.fillRect(0, canvas.height/2 - 3, canvas.width, 6);
   ctx.restore();
+}
+function clickModal(e) {
+  if(e.target.closest('[data-target="replay"]')) location.reload();
+  // if(e.target.closest('[data-target="menu"]')) window.location.href = 'URL2';
+  
 }
