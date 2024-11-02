c3e34bfdc28fbf1ee2848d5c92b5bb614772da07
Beslan05 <beslan.bakhishev@mail.ru>
2024-11-02 14:19:10 +0300

    внесены изменения

--- server/server.js
+++ server/server.js
@@ -10,11 +10,7 @@ server.listen(3000,listen)
 const html = fs.readFileSync('../client/index.html','UTF-8');
 let staticPath = path.join(__dirname, '..', 'client');
 server.use(express.static(staticPath));
-server.get('/',start);
 
-function start(req, res)  {
-  res.send('main')
-}
 function listen()  {
   log('сервак слушает...')
 }
